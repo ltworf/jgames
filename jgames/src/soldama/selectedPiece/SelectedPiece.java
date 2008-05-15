fbebecb1a5e48235e0cc712d4f69d3028d99375e
LtWorf <LtWorf@158644bf-b6ea-47f4-bc45-214f26edb3e9>
2008-05-15 13:27:37 +0000

    git-svn-id: http://galileo.dmi.unict.it/svn/jgames@6 158644bf-b6ea-47f4-bc45-214f26edb3e9

---
+++ jgames/src/soldama/selectedPiece/SelectedPiece.java
@@ -0,0 +1,21 @@
+/*
+ * Created on 30-ott-2004
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package soldama.selectedPiece;
+
+/**
+ * @author Salvo Tomaselli
+ *
+ * This class specify the selected piece... nothing else!
+ */
+public class SelectedPiece {
+    public boolean selected=false;
+    public byte x=0;
+    public byte y=0;
+    public byte remaining;
+    public byte eat=0;
+
+}
