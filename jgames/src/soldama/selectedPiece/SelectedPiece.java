a20664c339be2ca08b1facd4017fb90a562da65e
LtWorf <LtWorf@158644bf-b6ea-47f4-bc45-214f26edb3e9>
2008-05-15 13:31:28 +0000

    added license
    git-svn-id: http://galileo.dmi.unict.it/svn/jgames@8 158644bf-b6ea-47f4-bc45-214f26edb3e9

--- jgames/src/soldama/selectedPiece/SelectedPiece.java
+++ jgames/src/soldama/selectedPiece/SelectedPiece.java
@@ -1,9 +1,24 @@
 /*
- * Created on 30-ott-2004
- *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+jgames
+Copyright (C) 2007  Salvo "LtWorf" Tomaselli
+
+Weborf is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+@author Salvo "LtWorf" Tomaselli <ltworf@galileo.dmi.unict.it>
+
  */
+
 package soldama.selectedPiece;
 
 /**
