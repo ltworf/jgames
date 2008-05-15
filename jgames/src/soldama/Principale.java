c82c225e16a567c5d55f6def5ffac29f90391b20
LtWorf <LtWorf@158644bf-b6ea-47f4-bc45-214f26edb3e9>
2008-05-15 13:27:10 +0000

    git-svn-id: http://galileo.dmi.unict.it/svn/jgames@5 158644bf-b6ea-47f4-bc45-214f26edb3e9

---
+++ jgames/src/soldama/Principale.java
@@ -0,0 +1,23 @@
+package soldama;
+
+/**
+ * @author Salvo Tomaselli
+ * @link http://supersalvus.altervista.org
+ */
+public class Principale {
+
+	public static void main(String[] args) {
+	    
+	    System.out.println("Solitario Dama by Salvo Tomaselli");
+	    System.out.println("	tiposchi@users.sourceforge.net");
+	    System.out.println("	http://supersalvus.altervista.org");
+	    System.out.println("	https://sourceforge.net/projects/smallgamesc/");
+	    
+	    System.out.println ("Solitario Dama, Copyright (C) 2005 Salvo Tomaselli\n	    Solitario Dama comes with ABSOLUTELY NO WARRANTY; for details see GPL license");
+	    
+		Solitario window = new Solitario();
+
+		
+		
+	}
+}
